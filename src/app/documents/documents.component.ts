8e6d9caa615f2e7d450c2f15fc6cf7d09d8674d1
Chazona Baum <chznbaum@gmail.com>
2017-10-02 16:02:26 -0400

    built out interface for modeling docs

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Document } from './document';
 
 @Component({
   moduleId: module.id,
@@ -6,4 +7,31 @@ import { Component } from '@angular/core';
   templateUrl: './documents.component.html',
   styleUrls: ['./documents.component.css']
 })
-export class DocumentsComponent {}
+export class DocumentsComponent {
+  documents: Document[] = [
+    {
+      title: 'My First Document',
+      description: 'The first sample document used by the front end.',
+      file_url: 'https://google.com',
+      updated_at: '09/30/2017',
+      image_url: 'https://google.com',
+      client_id: '1'
+    },
+    {
+      title: 'My Second Document',
+      description: 'The second sample document used by the front end.',
+      file_url: 'https://google.com',
+      updated_at: '10/01/2017',
+      image_url: 'https://google.com',
+      client_id: '1'
+    },
+    {
+      title: 'My Third Document',
+      description: 'The third sample document used by the front end.',
+      file_url: 'https://google.com',
+      updated_at: '10/02/2017',
+      image_url: 'https://google.com',
+      client_id: '1'
+    }
+  ]
+}
