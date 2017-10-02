882f25b21b3dcdc686c72b8e3a2ebaa378ab6df5
Chazona Baum <chznbaum@gmail.com>
2017-10-02 16:21:28 -0400

    created proposal-list component

---
+++ src/app/proposal/proposal-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  moduleId: module.id,
+  selector: 'proposal-list',
+  templateUrl: './proposal-list.component.html',
+  styleUrls: ['./proposal-list.component.css']
+})
+export class ProposalListComponent {}
