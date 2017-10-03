afd08c255e72f2d867f43fd7473dffc305cce982
Chazona Baum <chznbaum@gmail.com>
2017-10-02 21:34:05 -0400

    created proposal-new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { DocumentsComponent } from './documents/documents.component';
-import { ProposalListComponent } from './proposal/proposal-list.component'
+import { ProposalListComponent } from './proposal/proposal-list.component';
+import { ProposalNewComponent } from './proposal/proposal-new.component';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -12,11 +14,13 @@ import { AppRoutingModule } from './app-routing.module';
   declarations: [
     AppComponent,
     HomepageComponent,
-    DocumentsComponent, ProposalListComponent
+    DocumentsComponent, ProposalListComponent,
+    ProposalNewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
